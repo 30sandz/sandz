8463b1f778d944ddd29d21a21e7ca2949ccff5c0
Sandeep <100069895+30sandz@users.noreply.github.com>
2024-01-07 17:40:00 +0530

    Checking

---
+++ main.js
@@ -0,0 +1,16 @@
+function anime() {
+    let text = document.getElementsByClassName("anime");
+}
+
+
+
+
+function Drop() {
+    var txt = document.getElementById("drop-con");
+    var txt1 = document.getElementById("drop-menu");
+    txt.style.visibility = (txt.style.visibility === "hidden")?"visible":"hidden";
+    txt.style.top = (txt.style.top === "-1vh")?"0vh":"-1vh";
+
+    
+
+}
